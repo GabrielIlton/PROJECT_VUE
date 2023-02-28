51bc689846462af2d27566959fbb8774223881fd
Gabriel Ilton <gabrielssantosjau@gmail.com>
2023-02-27 22:44:34 -0300

    feat: add button come back in title

--- vue_project/src/components/Share/title.vue
+++ vue_project/src/components/Share/title.vue
@@ -1,17 +1,37 @@
 <template>
     <div>
-        <h1>{{text}}</h1>
+        <div style="display: flex; width: 50%">
+            <h1>{{text}}</h1>
+            <div class="comeBack">
+                <slot></slot>
+                <button v-show="btnComeBack" class="btn btnReturn" @click="comeBack()">Voltar</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            text: String
+            text: String,
+            btnComeBack: Boolean
+        },
+        methods: {
+            comeBack() {
+                this.$router.back()
+            }
         }
     }       
 </script>
 
 <style scoped>
-
+    .comeBack {
+        width: 50%;
+        position: absolute;
+        margin-top: 15px;
+    }
+    .btnReturn {
+        float: right;
+        background-color: rgb(199, 199, 199);
+    }
 </style>
