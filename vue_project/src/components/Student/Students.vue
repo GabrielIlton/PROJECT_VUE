6d22fc3b759be9be036e55f0f3d7f300ee2f2f41
Gabriel Ilton <gabrielssantosjau@gmail.com>
2023-02-27 22:45:01 -0300

    feat: improvements to list students with professors

--- vue_project/src/components/Student/Students.vue
+++ vue_project/src/components/Student/Students.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <titulo text="Aluno"/>
-    <div>
+    <titulo :text="professorId ? `Professor: ${professor.name}` : 'Todos os Alunos'" :btnComeBack="true"/>
+    <div v-if="professorId">
       <input type="text" placeholder="Nome do Aluno" v-model="name" @keyup.enter="add()">
       <button class="btn btnInput" @click="add()">Adicionar</button>
     </div>
@@ -14,9 +14,11 @@
       </thead>
       <tbody v-if="students.length">
         <tr v-for="(student, index) in students" :key="index">
-          <td>{{student.id}}</td>
-          <td>{{student.name}} {{student.surname}}</td>
-          <td><button class="btn btnDanger" @click="remove(student)">Remover</button></td>
+          <td class="colSmall">{{student.id}}</td>
+          <router-link :to="`/aluno/${student.id}`" tag="td" style="cursor: pointer;">
+            {{student.name}} {{student.surname}}
+          </router-link>
+          <td class="colSmall"><button class="btn btnDanger" @click="remove(student)">Remover</button></td>
         </tr>
       </tbody>
       <tfoot v-else>
@@ -27,48 +29,70 @@
 </template>
 
 <script>
-import titulo from '../Share/title.vue'
+import Titulo from '../Share/title.vue'
 
 export default {
   components: {
-    titulo
+    Titulo
   },
   data() {
     return {
+      professorId: this.$route.params.professor_id,
       name: '',
+      professor: {
+
+      },
       students: []
     }
   },
   created() {
-    this.$http
-      .get('http://localhost:3000/students')
-      .then(response => response.json())
-      .then(students => this.students = students)
-  },
-methods: {
-  add() {
-    const student = {
-      name: this.name,
-      surname: '',
+    if (this.professorId) {
+      this.getProfessors()
+      this.$http
+        .get(`http://localhost:3000/students?professor.id=${this.professorId}`)
+        .then(response => response.json())
+        .then(students => this.students = students)
+    } else {
+      this.$http
+        .get('http://localhost:3000/students')
+        .then(response => response.json())
+        .then(students => this.students = students)
     }
-
-    this.$http
-      .post('http://localhost:3000/students', student)
-      .then(response => response.json())
-      .then(student => {
-        this.students.push(student);
-        this.name = '';
-      })
   },
-  remove(student) {
-    this.$http
-      .delete(`http://localhost:3000/students/${student.id}`)
-      .then(() => {
-        const index = this.students.indexOf(student);
-        this.students.splice(index, 1);
-      })
-  }
-},
+  methods: {
+    add() {
+      const student = {
+        name: this.name,
+        surname: '',
+        professor: {
+          id: this.professor.id,
+          name: this.professor.name
+        }
+      }
+
+      this.$http
+        .post('http://localhost:3000/students', student)
+        .then(response => response.json())
+        .then(student => {
+          this.students.push(student);
+          this.name = '';
+        })
+    },
+    remove(student) {
+      this.$http
+        .delete(`http://localhost:3000/students/${student.id}`)
+        .then(() => {
+          const index = this.students.indexOf(student);
+          this.students.splice(index, 1);
+        })
+    },
+    getProfessors() {
+      this.$http
+        .get(`http://localhost:3000/professors/${this.professorId}`)
+        .then(response => response.json())
+        .then(professor => this.professor = professor)
+    }
+  },
   props: {
   }
 }
@@ -76,6 +100,7 @@ methods: {
 
 <style scoped>
   input {
+    width: calc(100% - 195px);
     border: 0px;
     padding: 20px;
     font-size: 1.3em;
@@ -83,6 +108,7 @@ methods: {
     display: inline;
   }
   .btnInput {
+    width: 150px;
     display: inline;
     border: 0px;
     padding: 20px;
@@ -93,5 +119,8 @@ methods: {
     padding: 20px;
     margin: 0px;
     border: 0px;
+  }
+  .colSmall {
+    width: 15%;
   }
 </style>
