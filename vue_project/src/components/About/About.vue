b12c154b9b8c59485a8419df1d350515f9ebf66f
Gabriel Ilton <gabrielssantosjau@gmail.com>
2023-02-27 22:42:57 -0300

    feat: add about

---
+++ vue_project/src/components/About/About.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <titulo text="Sobre" />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus earum a quidem quae maiores accusamus, recusandae rerum molestiae illo iusto voluptatum, laudantium, officiis dolores praesentium odio modi nulla omnis. Quam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consequatur quo dolores perspiciatis explicabo ipsa corporis dicta fugit illum perferendis. Consectetur et ad cupiditate illum, deleniti exercitationem earum? Provident, assumenda.</p>
+    </div>
+</template>
+
+<script>
+    import Titulo from '../Share/title.vue'
+    export default {
+        components: {
+            Titulo
+        }
+    }   
+</script>
+
+<style scoped>
+
+</style>
